aba3b1a760bf9ac56bffcbd3288455becce445d5
Devjoks <devjoks@gmail.com>
2017-09-12 20:33:43 -0300

    Conexion a Firebase de About
    Datos en Firebase

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -6,13 +6,29 @@ export class InformacionService {
 
   info: any = {};
   cargada = true;
+  carga_about = true;
+  equipo: any[] = [];
 
   constructor( public http: Http ) {
+
+    this.carga_info();
+    this.carga_sobre_nosotrs();
+
+  }
+
+  public carga_info() {
     this.http.get('assets/data/info.data.json')
-      .subscribe( data => {
-          console.log(data.json());
-          this.cargada = true;
-          this.info = data.json();
-      });
+    .subscribe( data => {
+        this.cargada = true;
+        this.info = data.json();
+    });
+  }
+  public carga_sobre_nosotrs() {
+    this.http.get('https://htmltoangular-portafolio.firebaseio.com/team.json')
+    .subscribe( data => {
+        console.log(data.json());
+        this.carga_about = true;
+        this.equipo = data.json();
+    });
   }
 }
