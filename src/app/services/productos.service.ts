38a17bdb9049b7449ad63f8aaa5a0002a9b087a2
Devjoks <devjoks@gmail.com>
2017-09-12 21:39:27 -0300

    Carga de imagenes por codigo

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -15,14 +15,15 @@ export class ProductosService {
 
   public cargar_productos() {
 
-    this.cargando = false;
+    this.cargando = true;
 
     if ( this.productos.length === 0 ) {
 
       this.http.get('https://htmltoangular-portafolio.firebaseio.com/productos_idx.json')
                 .subscribe( res => {
                   console.log( res.json() );
-                  this.cargando = true;
+                    this.cargando = false;
+                    this.productos = res.json();
                 });
     }
   }
